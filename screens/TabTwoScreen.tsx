7f5cb95d84c5c8c8c7028e2b75e825d56df8dd5a
nkusikevin <nkusikvnhart@gmail.com>
2022-11-12 20:48:19 +0200

    init push not

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,31 +1,47 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import React, { useEffect } from "react";
+import { StyleSheet } from "react-native";
+import messaging from "@react-native-firebase/messaging";
+import { Text, View } from "../components/Themed";
 
 export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
+	// const requestPermission = async () => {
+	// 	const authStatus = await messaging().requestPermission();
+	// 	const enabled =
+	// 		authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+	// 		authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+	// 	if (enabled) {
+	// 		console.log("Authorization status:", authStatus);
+	// 	}
+	// };
+
+	// useEffect(() => {
+	// 	//@ts-ignore
+	// 	if (requestPermission()) {
+	// 		messaging()
+	// 			.getToken()
+	// 			.then((token) => {
+	// 				console.log(token);
+	// 			});
+	// 	}
+	// }, []);
+
+	return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: "80%",
+	},
 });
